ebaed3fefae68a5eac517cafdc260d8106531bf2
Rafael do Amaral Costa <rafaelcostatricolor@gmail.com>
2025-08-19 21:37:37 -0300

    initial config

---
+++ efood/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles"
+
+export const Home = () => {
+
+    return(
+            <S.Header>
+            
+                    <p>aaaaaa</p>
+                
+
+            </S.Header>
+    )
+}
---
+++ efood/src/pages/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+    width: 100vw;
+    background-image:  url("src/assets/fundo.png");
+    color: red;
+    top:0;
+    p{
+        color: green;
+    }
+
+`
