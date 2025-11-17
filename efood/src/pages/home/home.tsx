dce8be970ba46c168d348cb05377e05001d43d4f
Rafael do Amaral Costa <rafaelcostatricolor@gmail.com>
2025-11-17 00:49:49 -0300

    hotfix: removing unused declarations from home.tsx

--- efood/src/pages/home/home.tsx
+++ efood/src/pages/home/home.tsx
@@ -1,12 +1,11 @@
 import * as S from "./styles"
 import { useSelector, useDispatch } from "react-redux";
-import { selectRestaurantes, selectRestaurantesLoading, selectRestaurantesError } from "../../redux/selectorsRestaurante";
+import { selectRestaurantes} from "../../redux/selectorsRestaurante";
 import { fetchRestaurantes } from "../../redux/actionRestaurante";
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Header } from "../../components/Header/Header"
 import { Footer } from "../../components/Footer/Footer"
 import { DivItem } from "../../components/Item/Item"
-import type { Item } from "../../types/item"
 import type { Restaurante } from "../../types/restaurante";
 import type { AppDispatch } from "../../redux/store";
 
@@ -15,8 +14,6 @@ export const Home = () => {
     const dispatch = useDispatch<AppDispatch>();
 
     const restaurantes:Restaurante[]  = useSelector(selectRestaurantes);
-    const loading = useSelector(selectRestaurantesLoading);
-    const error = useSelector(selectRestaurantesError);
 
     useEffect(() => {
         dispatch(fetchRestaurantes());
